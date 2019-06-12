0a060b1aab29ff7dbf9d5a552bf47ff6f32eaf73
Nox <juan.angarita.11@gmail.com>
2019-06-12 18:59:36 -0500

    Template divied

---
+++ components/layout/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <p><span class="copyleft">&copy;</span>Nox {{ year }}</p>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+}
+</script>
