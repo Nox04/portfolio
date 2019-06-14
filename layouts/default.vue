8b14ff7fa1d8f00139adc009139fdad6332f06c4
Nox <juan.angarita.11@gmail.com>
2019-06-13 20:47:49 -0500

    Final clean

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,6 @@
 
 <style lang="scss">
 @import '~/static/css/bootstrap.css';
-@import '~/static/css/fluidbox.min.css';
 @import '~/static/css/ionicons.css';
 @import '~/static/css/responsive.css';
 @import '~/static/css/styles.css';
