d0f37a643a3f367f1bff8ad5558d22efd0438dd3
Nox <juan.angarita.11@gmail.com>
2019-06-13 20:08:00 -0500

    Fix css and about layout

--- components/sections/about/About.vue
+++ components/sections/about/About.vue
@@ -17,44 +17,41 @@
           <p class="margin-b-30">{{ $t('bio') }}</p>
           <p class="margin-b-50">{{ $t('bio2') }}</p>
           <div class="row">
-            <div class="col-xs-6 col-md-6 col-lg-2 text-center">
+            <div class="col-6 col-sm-6 col-md-6 col-lg-2 text-center">
               <spinner :progress="93">
                 <p class="progress-title">Laravel</p>
               </spinner>
             </div>
-            <!-- col-sm-6-->
 
-            <div class="col-xs-6 col-md-6 col-lg-2 text-center">
+            <div class="col-6 col-sm-6 col-md-6 col-lg-2 text-center">
               <spinner :progress="91">
                 <p class="progress-title">Vue.js</p>
               </spinner>
             </div>
 
-            <div class="col-xs-6 col-md-6 col-lg-2 text-center">
+            <div class="col-6 col-sm-6 col-md-6 col-lg-2 text-center">
               <spinner :progress="85">
                 <p class="progress-title">R. Native</p>
               </spinner>
             </div>
 
-            <div class="col-sm-6 col-md-6 col-lg-2 text-center">
+            <div class="col-6 col-sm-6 col-md-6 col-lg-2 text-center">
               <spinner :progress="84">
                 <p class="progress-title">Java</p>
               </spinner>
             </div>
-            <!-- col-sm-6-->
 
-            <div class="col-sm-6 col-md-6 col-lg-2 text-center">
+            <div class="col-6 col-sm-6 col-md-6 col-lg-2 text-center">
               <spinner :progress="70">
                 <p class="progress-title">Kotlin</p>
               </spinner>
             </div>
 
-            <div class="col-sm-6 col-md-6 col-lg-2 text-center">
+            <div class="col-6 col-sm-6 col-md-6 col-lg-2 text-center">
               <spinner :progress="55">
                 <p class="progress-title">C++</p>
               </spinner>
             </div>
-            <!-- col-sm-6-->
           </div>
           <!-- row -->
         </div>
