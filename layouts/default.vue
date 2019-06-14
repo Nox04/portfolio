27887a7cc5ec9f1e512a2652dd86879a6d32ab05
Nox <juan.angarita.11@gmail.com>
2019-06-13 20:31:22 -0500

    Revert "Let's test"
    This reverts commit 34214a69b8fd1a7a70b3c638bf62d80161addb5d.

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,8 @@
 </template>
 
 <style lang="scss">
-@import '~bootstrap/scss/bootstrap.scss';
+@import '~/static/css/bootstrap.css';
+@import '~/static/css/fluidbox.min.css';
 @import '~/static/css/ionicons.css';
 @import '~/static/css/responsive.css';
 @import '~/static/css/styles.css';
