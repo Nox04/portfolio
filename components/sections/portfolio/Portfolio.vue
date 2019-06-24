37945568d8e5ddc34a77e1adeb768fbe0746c792
Nox <juan.angarita.11@gmail.com>
2019-06-24 01:37:43 -0500

    Add html anchor to portfolio section

--- components/sections/portfolio/Portfolio.vue
+++ components/sections/portfolio/Portfolio.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="portfolio-section section">
+  <section id="portfolio" class="portfolio-section section">
     <div class="container">
       <div class="row">
         <div class="col-sm-12 col-md-3">
