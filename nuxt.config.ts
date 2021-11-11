f7b2a33e247ee662b747c98600ea068f99d1a407
Nox <juan.angarita.11@gmail.com>
2021-11-11 12:16:00 -0500

    Fix build Nuxt3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 import { defineNuxtConfig } from 'nuxt3'
 const config = defineNuxtConfig({
-  target: 'static',
+  nitro: {
+    preset: 'server',
+  },
   css: ['~/static/styles/styles.scss'],
   build: {
     postcss: {
@@ -12,52 +14,10 @@ const config = defineNuxtConfig({
       },
     },
   },
-
-  buildModules: [
-    // [
-    //   '@nuxtjs/google-analytics',
-    //   {
-    //     id: 'UA-158523703-1'
-    //   }
-    // ]
-  ],
-
   /*
    ** Customize the progress-bar color
    */
   loading: { color: '#00adb5' },
-
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // [
-    //   'nuxt-i18n',
-    //   {
-    //     locales: [
-    //       { code: 'en', iso: 'en-US', file: 'en.ts', name: 'English' },
-    //       { code: 'es', iso: 'es-CO', file: 'es.ts', name: 'Spanish' }
-    //     ],
-    //     langDir: 'i18n/',
-    //     lazy: true,
-    //     defaultLocale: 'en',
-    //     vueI18n: { fallbackLocale: 'en' },
-    //     vuex: false
-    //   }
-    // ],
-    // '@nuxtjs/sitemap'
-  ],
-  // sitemap: {
-  //   path: '/sitemap.xml',
-  //   hostname: 'https://juanangarita.com',
-  //   cacheTime: 1000 * 60 * 60 * 2,
-  //   trailingSlash: true,
-  //   gzip: true
-  // },
-  // manifest: {
-  //   name: 'Juan Angarita',
-  //   lang: 'es-CO'
-  // }
 })
 
 export default config
